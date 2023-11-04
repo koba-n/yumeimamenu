4544bb8bd9aa913a0f14ea5797dcc8c3d5c16acb
Naoto Kobayashi <naoto.kobayashi.729@ctc-g.co.jp>
2023-11-04 17:19:52 +0900

    first commit

--- menu.py
+++ menu.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 @app.route('/')
 def menu():
     # menu.htmlテンプレートをレンダリングし、メニューデータを渡す
-    return render_template('menu.html')
+    return render_template('menuyume.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
